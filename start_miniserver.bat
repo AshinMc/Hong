6bd441a16961bb19a8d4d59e3bb5c7f8783431a2
AshinMc <ashinalwin3@gmail.com>
2025-07-15 18:36:31 +0200

    created a mini server
    with minimal code and logging, basically removed a lot of stuff

---
+++ start_miniserver.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Starting Hong Game Environment...
+
+echo.
+echo Starting Python WebSocket Server...
+start cmd /k python mini_server\mini_server.py
+
+echo.
+echo Environment Started! Please connect both players.
